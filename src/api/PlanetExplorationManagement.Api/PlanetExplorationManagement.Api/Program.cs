04d7256b148178006c9f791a2dace6181b04793b
Iulian Peiu <iulianpeiu6@gmail.com>
2023-11-30 15:13:13 +0200

    Add build_and_test workflow action. Init PlanetExplorationManagement Api.

---
+++ src/api/PlanetExplorationManagement.Api/PlanetExplorationManagement.Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapHealthChecks("/health");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
